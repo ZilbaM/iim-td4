beabdaec212f78e31be3c122a6b7cb9ac3769ace
Basile Maille <basmaille59@gmail.com>
2023-07-04 12:12:25 +0200

    fix linter script

--- tests/getRandomNumber.test.js
+++ tests/getRandomNumber.test.js
@@ -1,4 +1,4 @@
-const getRandomNumber = require('../index.js');
+const getRandomNumber = require('../src/index.js');
 
 describe('getRandomNumber', () => {
   test('returns a random number between 0 and 100', () => {
